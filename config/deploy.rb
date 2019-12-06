02f80d4e15d430466d1ea71e168faf4c6174721e
andreyaksenov <andrey.aksenov@jetbrains.com>
2019-12-06 11:27:16 +0300

    remove config/secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :application, "sample_rails_app"
 set :repo_url, "https://github.com/JetBrains/sample_rails_app"
 set :branch, "capistrano-deploy"
 set :deploy_to, "/home/deploy/#{fetch :application}"
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, "config/database.yml"
 append :linked_dirs, ".bundle", "tmp", "log"
 set :keep_releases, 5
 set :format, :pretty
