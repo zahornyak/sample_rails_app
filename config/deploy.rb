e426acf065e0246284051b1d4c1014c163a27209
andreyaksenov <andrey.aksenov@jetbrains.com>
2019-12-06 11:43:44 +0300

    remove linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,6 @@ set :application, "sample_rails_app"
 set :repo_url, "https://github.com/JetBrains/sample_rails_app"
 set :branch, "capistrano-deploy"
 set :deploy_to, "/home/deploy/#{fetch :application}"
-append :linked_files, "config/database.yml", "config/master.key"
-append :linked_dirs, ".bundle", "tmp", "log"
+append :linked_files, "config/master.key"
 set :keep_releases, 5
 set :format, :pretty
