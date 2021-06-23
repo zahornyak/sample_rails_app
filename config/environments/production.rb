e6eca6372b20cdb35b2d5350e761bd94c471dbf7
Andriy Zagorniak <andriyzzahornyak@gmail.com>
2021-06-24 00:20:53 +0300

    http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://mysterious-atoll-47182.herokuapp.com'
+  host = 'http://18.159.248.236'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
